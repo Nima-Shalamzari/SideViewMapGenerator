4874cd7f880c0624294a87df7a035052b21db200
Percy <nimakto@gmail.com>
2024-06-19 14:54:04 +0330

    Pesudo cave generation

--- Assets/Scripts/RTProceduralGeneration.cs
+++ Assets/Scripts/RTProceduralGeneration.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Rendering.Universal;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -10,10 +11,14 @@ public class RTProceduralGeneration : MonoBehaviour
     [SerializeField] int width;
     [SerializeField] int height;
     [SerializeField] float smoothness;
+    int[] perlinHeightArray;
 
     [Header("Cave Gen")]
-    [Range(0,1)]
-    [SerializeField] float modifier;
+    //[Range(0,1)]  for perlin noise cave we need thses two lines of code
+    //[SerializeField] float modifier;
+    [Range(0,100)]
+    [SerializeField] int randomFillPercent;
+    [SerializeField] int smoothAmount;
 
     [Header("Tile")]
     [SerializeField] TileBase groundTile;
@@ -26,9 +31,8 @@ public class RTProceduralGeneration : MonoBehaviour
     int[,] map;
 
     private void Start() {
-        map = GenerateArray(width, height, true);
-        map = TerrainGeneration(map);
-        RenderMap(map, groundTileMap, caveTileMap, groundTile, caveTile);
+        perlinHeightArray = new int[width];
+        Generation();
     }
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
@@ -37,10 +41,11 @@ public class RTProceduralGeneration : MonoBehaviour
     }
 
     private void Generation() {
-        seed = Time.time;
+        //seed = Time.time;
         clearMap();
         map = GenerateArray(width, height, true);
         map = TerrainGeneration(map);
+        SmoothMap(smoothAmount);
         RenderMap(map, groundTileMap, caveTileMap, groundTile, caveTile);
     }
 
@@ -55,17 +60,58 @@ public class RTProceduralGeneration : MonoBehaviour
     }
 
     public int[,] TerrainGeneration(int[,] map) {
+        System.Random pesudoRandom = new System.Random(seed.GetHashCode());
         int perlinHeight;
         for (int x = 0; x < width; x++) {
             perlinHeight = Mathf.RoundToInt(Mathf.PerlinNoise(x / smoothness, seed) * height/2);
             perlinHeight += height / 2;
+            perlinHeightArray[x] = perlinHeight;
             for (int y = 0; y < perlinHeight; y++) {
-                //map[x, y] = 1;
-                int caveValue = Mathf.RoundToInt(Mathf.PerlinNoise((x * modifier) + seed, (y * modifier) + seed));
-                map[x, y] = (caveValue == 1)? 2 : 1;
+                //map[x, y] = 1; we use this line for ground generation only
+                //int caveValue = Mathf.RoundToInt(Mathf.PerlinNoise((x * modifier) + seed, (y * modifier) + seed)); we use these two lines for using perlin noise to generate caves.
+                //map[x, y] = (caveValue == 1)? 2 : 1;
+                map[x, y] = (pesudoRandom.Next(1, 100) < randomFillPercent) ? 1 : 2;
             }
         }
         return map;
+    }
+
+    void SmoothMap( int smoothAmount) {
+        for (int i = 0; i < smoothAmount; i++) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < perlinHeightArray[x]; y++) {
+                    if (x == 0 || y == 0 || x == width - 1 || y == perlinHeightArray[x] - 1) {
+                        map[x, y] = 1;
+                    }
+                    else {
+                        int surroundingGroundCount = GetSurroundingGroundCount(x, y);
+                        if (surroundingGroundCount > 4) {
+                            map[x, y] = 1;
+                        }
+                        else if (surroundingGroundCount < 4) {
+                            map[x, y] = 2;
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    int GetSurroundingGroundCount(int gridx, int gridy) {
+        int groundCount = 0;
+        for (int nebx = gridx-1; nebx <= gridx+1; nebx++) {
+            for (int neby = gridy-1; neby <= gridy+1; neby++) {
+                if (nebx >= 0 && nebx < width && neby >= 0 && neby < height) { //shouldn't be out of neightbourhood zone
+                    if (nebx != gridx || neby != gridy) { //shouldn't be the middle tile
+                        if (map[nebx,neby] == 1) {
+                            groundCount++;
+                        }
+                    }
+                }
+            }
+        }
+        return groundCount;
     }
 
     public void RenderMap(int[,] map, Tilemap groundTileMap, Tilemap caveTileMap, TileBase groundTilebase, TileBase caveTile) {
