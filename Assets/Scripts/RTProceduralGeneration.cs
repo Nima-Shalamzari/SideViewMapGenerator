71b872fa122b8bd224535fedcb803a4f3ae37ee2
Percy <nimakto@gmail.com>
2024-08-10 16:29:19 +0330

    Last Edits
    -Excessive lines and variables remove.
    -Documentations added.
    -water physics class removed

--- Assets/Scripts/RTProceduralGeneration.cs
+++ Assets/Scripts/RTProceduralGeneration.cs
@@ -12,30 +12,27 @@ using UnityEngine.Tilemaps;
 public class RTProceduralGeneration : MonoBehaviour
 {
     [Header("Terrain Gen")]
-    [SerializeField] int width;
-    [SerializeField] int height;
-    [SerializeField] float smoothness;
-    [SerializeField] int WaterLevel;
-    int[] perlinHeightArray;
+    [SerializeField] int width; // width of our map
+    [SerializeField] int height; // height of our map
+    [SerializeField] float smoothness; // a floating number that smoothes perlin noise
+    int WaterLevel; // water level at surface
+    int[] perlinHeightArray; // an array to hold floating surface ground height with the length of width
 
     [Header("Mineral Gen")]
-    //Fill here with some info
-    [Range(0, 1)]
-    [SerializeField] float randomStoneFillPercent;
+    float randomStoneFillPercent; // percentage of stone we want our map to have - mostly useless for now
 
     [Header("Cave Gen")]
-    [Range(0,1)]//  for perlin noise cave we need thses two lines of code
-    [SerializeField] float modifier;
+    [Range(0,1)]
+    [SerializeField] float modifier; // a modifier number used in perlin noise
     [Range(0,100)]
-    [SerializeField] int randomFillPercent;
-    [SerializeField] int smoothAmount;
+    [SerializeField] int randomFillPercent; // percentage of ground tile compare to cave
+    [SerializeField] int smoothAmount; //number of times we soften pesudo random generated ground and caves
 
-    [Header("Water Gen")]
-    LinkedListStack<Coordinates> mapList = new LinkedListStack<Coordinates>();
-    private int minWater, maxWater, randHeight;
+    //[Header("Water Gen")]
+    LinkedListStack<Coordinates> mapList = new LinkedListStack<Coordinates>(); // a linkedlist-stack that we use for traversing caves and helps water generation in caves
+    private int minWater, maxWater, randHeight; // used for cave water generation
 
-    [Header("Surface environment Gen")]
-
+    //[Header("Surface environment Gen")]
 
     [Header("Tile")]
     [SerializeField] TileBase groundTile;
@@ -50,54 +47,52 @@ public class RTProceduralGeneration : MonoBehaviour
     [SerializeField] Tilemap environmentTileMap;
 
     [Header("Tile")]
-    [SerializeField] float seed;
-    [SerializeField] float scale;
-    [SerializeField] int octaves;
-    [SerializeField] float persistance;
-    [SerializeField] float lacunarity;
-    float maxNoiseHeight, minNoiseHeight;
+    [SerializeField] float seed; // seed of our generated maps
+    [SerializeField] float scale; // used for scaling the Perlin noise
+    [SerializeField] int octaves; // used for number of octaves of Perlin noise
+    [SerializeField] float persistance; // used for persistance of the Perlin noise
+    [SerializeField] float lacunarity; // used for lacunarity of the Perlin noise
+    float maxNoiseHeight, minNoiseHeight; // used to determine the minimum and maximum height of perlin noise values so later we can later lock them between zero and one
 
-    protected int[,] map;
-    private int tempGroundCheck;
+    protected int[,] map; // a two dimensional array that holds values and numbers of each element on the map
+    private int tempGroundCheck; // a temporary number that checks if number of grounds around a block is more than stones and is used in smoothing function
 
     private void Start() {
         perlinHeightArray = new int[width];
         Generation();
-        
     }
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             Generation();
         }
     }
-
     private void Generation() {
-        //seed = Time.time;
         clearMap();
-        map = GenerateArray(width, height, true);
-        map = TerrainGeneration(map);
-        SmoothMap(smoothAmount);
-        map = stoneGeneration(map);
-        map = caveAnalysis(map);
-        map = surfaceWaterLevel(map);
-        map = treeGeneration(map);
-        RenderMap(map, groundTileMap, caveTileMap,waterTileMap, environmentTileMap, groundTile, caveTile, waterTile, stoneTile, TreeTile);
+        map = GenerateArray(width, height, true); // declration of map array with zero or air
+        map = TerrainGeneration(map); // generating ground and cave
+        SmoothMap(smoothAmount); // smoothing caves
+        map = stoneGeneration(map); // generating stone layers and chunks
+        map = caveAnalysis(map); // analysing caves and generating water inside them
+        map = surfaceWaterLevel(map); // generating surface water
+        map = treeGeneration(map); // generating trees
+        RenderMap(map, groundTileMap, caveTileMap,waterTileMap, environmentTileMap, groundTile, caveTile, waterTile, stoneTile, TreeTile); // rendering all the tiles and tile maps
     }
 
     public int[,] GenerateArray(int width, int height, bool empty) {
+        // *** function description : ***
+        // this function generates a two dimensional array with zeros only.
         int[,] map = new int[width, height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                
                 map[x, y] = (empty) ? 0 : 1;
             }
-        }
-        return map;
+        }return map;
     }
-
     public int[,] TerrainGeneration(int[,] map) {
+        // *** function description : ***
+        // this function uses pesudo random to generate ground and cave based on value of randomFillPercent.
         System.Random pesudoRandom = new System.Random(seed.GetHashCode());
-        System.Random pesudoStoneRandom = new System.Random((int) DateTime.Now.Ticks);
+        //System.Random pesudoStoneRandom = new System.Random((int) DateTime.Now.Ticks);
         int perlinHeight;
         //int maxHeight = 0;
         //int minHeight = height;
@@ -109,19 +104,19 @@ public class RTProceduralGeneration : MonoBehaviour
                 //map[x, y] = 1; we use this line for ground generation only
                 //int caveValue = Mathf.RoundToInt(Mathf.PerlinNoise((x * modifier) + seed, (y * modifier) + seed)); we use these two lines for using perlin noise to generate caves.
                 //map[x, y] = (caveValue == 1)? 2 : 1;
-                float stoneValue = Mathf.PerlinNoise((pesudoStoneRandom.Next(1, 100) * modifier) + seed, (y * modifier) + seed);
+                //float stoneValue = Mathf.PerlinNoise((pesudoStoneRandom.Next(1, 100) * modifier) + seed, (y * modifier) + seed);
                 map[x, y] = (pesudoRandom.Next(1, 100) < randomFillPercent) ? 1 : 2;
                 /*if (map[x,y] == 1) {
                     map[x, y] = ((stoneValue * 100) < randomStoneFillPercent) ? 6 : 1;
                 }*/ //activate this part to return back to default stone production.
                 //assist.GetSurroundingGround(x, y, minHeight, maxHeight);
-
             }
         }
         return map;
     }
-
     void SmoothMap( int smoothAmount) {
+        // *** function description : ***
+        // this function works based on surroundings of a block and decided whether there should be cave, ground or air.
         for (int i = 0; i < smoothAmount; i++) {
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < perlinHeightArray[x]; y++) {
@@ -145,10 +140,10 @@ public class RTProceduralGeneration : MonoBehaviour
                 }
             }
         }
-        
     }
-
     int GetSurroundingGroundCount(int gridx, int gridy) {
+        // *** function description : ***
+        // this function goes through surrounding blocks of a specific coordinate and collects information about air, ground and cave blocks.
         int groundCount = 0;
         int Ground = 0, Stone = 0;
         for (int nebx = gridx-1; nebx <= gridx+1; nebx++) {
@@ -169,18 +164,9 @@ public class RTProceduralGeneration : MonoBehaviour
         tempGroundCheck = (Ground >= Stone) ? 1 : 6;
         return groundCount;
     }
-
-    //private int[,] stoneGeneration(int[,] map) {
-
-    //    for (int x = 0; x < width; x++) {
-    //        for (int y = 0; y < height; y++) {
-    //            stoneGenerationAssist(map, x, y);
-    //        }
-    //    }
-    //    return map;
-    //}
-
     private int[,] stoneGeneration(int[,] map) {
+        // *** function description : ***
+        // this function uses the latest update of perline noise generation and generates three layers of stone horizontally and makes sure that we will always jave stone at the most button one and coming up, moderate to low amount stone have.
         float[,] noiseMap = new float[width, height];
         if (scale <= 0) scale = 0.0001f;
         maxNoiseHeight = float.MinValue;
@@ -247,25 +233,18 @@ public class RTProceduralGeneration : MonoBehaviour
                     return null;
             }
             for (int y = modifiedHeightStartPoint; y < modifiedHeight; y++) {
-                    //Debug.Log("noiseMap[" + X.ToString() + ", " + Y.ToString() + "] is equal to : " + noiseMap[X, Y]);
                     noiseMap[x,y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
-                    //Debug.Log("noiseMap[" + x.ToString() + ", " + y.ToString() + "] is equal to : " + noiseMap[x, y]);
                     if (noiseMap[x, y] > fillPercent && map[x, y] == 1) {
                         map[x, y] = 6;
                     }
-                    //Debug.Log("noiseMap["+X.ToString()+", "+Y.ToString()+"] is equal to : " + noiseMap[X, Y]);
                 }
             }
         }
-        
-        //Debug.Log("maxNoiseHeight is equal to : " + maxNoiseHeight);
-        //Debug.Log("minNoiseHeight is equal to : " + minNoiseHeight);
-        
-    
         return map;
     }
-
     int[,] caveAnalysis(int[,] map) {
+        // *** function description : ***
+        // this function traverses through map and finds caves and provide necessary variables for cave analysis assist function.
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 if (map[x, y] == 2) {
@@ -279,6 +258,8 @@ public class RTProceduralGeneration : MonoBehaviour
         return map;
     }
     void caveAnalysisAssistant(int[,] map, int x, int y) {
+        // *** function description : ***
+        // this function uses flood fill algorithm to travers each cave and mark it as a traversed cave and then goes through the same cave and based on a random hight, fill the cave with water.
         if (y < minWater) minWater = y;
         if (y > maxWater) maxWater = y;
         if (map[x, y] == 2) {
@@ -362,8 +343,9 @@ public class RTProceduralGeneration : MonoBehaviour
             caveAnalysisAssistant(map, coordi.X, coordi.Y);
         }
     }
-
     int[,] surfaceWaterLevel(int[,] map) {
+        // *** function description : ***
+        // this function based on a previously declared surface water level addes water on the surface of map.
         int waterLevel = WaterLevelDecleration(perlinHeightArray);
         for (int x = 0; x < width; x++) {
             for (int y = perlinHeightArray[x]; y <= waterLevel; y++) {
@@ -372,8 +354,9 @@ public class RTProceduralGeneration : MonoBehaviour
         }
         return map;
     }
-
     private int[,] treeGeneration(int[,] map) {
+        // *** function description : ***
+        // this function checks surface of ground and if there were no water on surface or stone instead ground and also if there were no tree in radious of 2 blocks around the targeted block, will generate tress in that location.
         bool collision = false;
         for (int x = 0; x < width; x++) {
             int y = perlinHeightArray[x];
@@ -393,9 +376,9 @@ public class RTProceduralGeneration : MonoBehaviour
         }
         return map;
     }
-   
-
     public void RenderMap(int[,] map, Tilemap groundTileMap, Tilemap caveTileMap,Tilemap waterTileMap, Tilemap environmentTileMap, TileBase groundTileBase, TileBase caveTileBase, TileBase waterTileBase, TileBase stoneTileBase, TileBase treeTileBase) {
+        // *** function description : ***
+        // this function applies map array into tile maps and translate them to tiles
         for (int x = 0; x< width; x++) {
             for (int y = 0; y < height; y++) {
                 if (map[x, y] == 1) {
@@ -412,21 +395,24 @@ public class RTProceduralGeneration : MonoBehaviour
             }
         }
     }
-
     private void clearMap() {
+        // *** function description : ***
+        // this function clears all tile maps and prepares screen for new generations.
         groundTileMap.ClearAllTiles();
         caveTileMap.ClearAllTiles();
         waterTileMap.ClearAllTiles();
         environmentTileMap.ClearAllTiles();
     }
-
     int RandomWaterHeight(int min, int max) {
+        // *** function description : ***
+        // this function calculates random water level in caves.
         System.Random pesudoRandom = new System.Random((int)DateTime.Now.Ticks);
         int randomHeight = pesudoRandom.Next(min - 1, max);
         return randomHeight;
     }
-
     private int WaterLevelDecleration(int[] perlinHeight) {
+        // *** function description : ***
+        // this function calculates surface water level based on understanding the amount minimum and maximum points of surface ground and reducing it to one third of it. 
         int perlinHeightMax = 0;
         int perlinHeightMin = height;
         for (int x = 0; x < width; x++) {
@@ -441,10 +427,11 @@ public class RTProceduralGeneration : MonoBehaviour
 }
 
 struct Coordinates {
+    // *** struct description : ***
+    // this struct helps to keep the coordinates of a location on map.
     public int X, Y;
     public Coordinates(int x, int y) {
         X = x; Y = y;
     }
-
 }
 
